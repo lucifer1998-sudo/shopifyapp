e5f6bcdeb3dfa6640d9bdd2d564383253259c3cf
Tanzeel <tanzeeltariq39@gmail.com>
2025-08-19 22:14:59 +0500

    working version

--- extensions/prescribery-checkout-ui/src/Checkout.jsx
+++ extensions/prescribery-checkout-ui/src/Checkout.jsx
@@ -4,20 +4,36 @@ import {
   Link,
   Text,
   useApi,
+  useAppMetafields,
 } from "@shopify/ui-extensions-react/checkout";
 export default reactExtension("purchase.thank-you.block.render", () => (
   <Extension />
 ));
 
 function Extension() {
-  const data = useApi();
-  const orderId = data.orderConfirmation.current.order.id; //gid://shopify/OrderIdentity/6747217789089
+  const { orderConfirmation } = useApi();
+  const metafields = useAppMetafields({
+    namespace: "custom",
+    key: "isprescribery",
+  });
+  const hasPrescribery = metafields.some(
+    (m) => m.metafield.key === 'isprescribery' && m.metafield.value === 'true'
+  );
+
+  console.log(hasPrescribery,'hasprescribery');
+  const orderId = orderConfirmation.current.order.id; //gid://shopify/OrderIdentity/6747217789089
 
   return (
-    <Banner title="Thank you for your order!">
-      <Text size="medium">
-        Please <Link to={`https://www.service.prescribery.com?id=${btoa(orderId)}`}> click here </Link> to submit the questionnaire
-      </Text>
-    </Banner>
+    hasPrescribery ? (
+      <Banner title="Thank you for your order!">
+        <Text size="medium">
+          Please{" "}
+          <Link to={`https://www.service.prescribery.com?id=${btoa(orderId)}`}>
+            click here
+          </Link>{" "}
+          to submit the questionnaire
+        </Text>
+      </Banner>
+    ) : null
   );
 }
